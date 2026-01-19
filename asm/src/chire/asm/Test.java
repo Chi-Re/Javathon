3cdebdecdd2dd9e8dfc81ce481e4b636b207aab6
炽热S <3531962676@qq.com>
2026-01-19 14:48:31 +0800

    限制调用范围

--- asm/src/chire/asm/Test.java
+++ asm/src/chire/asm/Test.java
@@ -1,7 +1,11 @@
 package chire.asm;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 public class Test {
     public static void main(String[] var0) {
-        System.out.println("aaa");
+        System.out.printf("aaa", "ddd", "fff");
     }
 }
--- asm/src/chire/asm/dynamic/builder/CallBuilder.java
+++ asm/src/chire/asm/dynamic/builder/CallBuilder.java
@@ -10,16 +10,34 @@ public class CallBuilder<T> extends Builder<T>{
         super(classAsm, type);
     }
 
-    public CallBuilder<T> callMethod(Class<?> owner, String var, Class<?>[] parameters, Class<?> returnType) {
-        classAsm.invokeMethod(Opcodes.INVOKEVIRTUAL, owner, var, Format.formatParameter(parameters, returnType));
+    public static class CallFunBuilder<T> extends Builder<T>{
+        public CallFunBuilder(ClassAsm classAsm, Class<T> type) {
+            super(classAsm, type);
+        }
 
-        return new CallBuilder<>(classAsm, this.type);
+        public CallBuilder<T> callMethod(Class<?> owner, String var, Class<?>[] parameters, Class<?> returnType) {
+            classAsm.invokeMethod(Opcodes.INVOKEVIRTUAL, owner, var, Format.formatParameter(parameters, returnType));
+
+            return new CallBuilder<>(classAsm, this.type);
+        }
     }
 
-    public CallBuilder<T> definitObj(Object obj) {
+    public CallFunBuilder<T> definitObj(Object obj) {
         classAsm.ldcInsn(obj);
 
-        return new CallBuilder<>(classAsm, type);
+        return new CallFunBuilder<>(classAsm, type);
+    }
+
+    public CallBuilder<T> call(int opcode, Class<?> owner, String var, Class<?> type) {
+        classAsm.invokeVar(opcode, owner, var, Format.formatPack(type)+";");
+
+        return new CallBuilder<>(classAsm,  this.type);
+    }
+
+    public CallBuilder<T> call(int opcode, Class<?> owner, String var, String type) {
+        classAsm.invokeVar(opcode, owner, var, type);
+
+        return new CallBuilder<>(classAsm,  this.type);
     }
 
     public T out(){
