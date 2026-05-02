cb3a74a131f1199247a39622e84aeb4b4ea54677
炽热S <3531962676@qq.com>
2026-05-02 18:17:02 +0800

    删除JPArgs多余注释(再次存档确保代码查找)

--- core/src/chire/python/lib/escape/JPArgs.java
+++ core/src/chire/python/lib/escape/JPArgs.java
@@ -1,10 +1,8 @@
 package chire.python.lib.escape;
 
-import chire.python.lib.PyList;
 import chire.python.lib.PyTuple;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class JPArgs {
     private final Map<String, Object> args = new LinkedHashMap<>();
@@ -15,36 +13,7 @@ public class JPArgs {
         this.keys = List.of(keys);
     }
 
-//    public void setArgs(JPArg... args) {
-//        boolean keyMode = false;
-//
-//        for (int i = 0; i < args.length; i++) {
-//            if (args[i].key != null) {
-//                keyMode = true;
-//
-//                this.args.put(args[i].key, args[i].object);
-//            } else if (keyMode) {
-//                throw new RuntimeException("no key");
-//            } else {
-//                this.args.put(this.keys.get(i), args[i].object);
-//            }
-//        }
-//    }
-
     public void setArgs(Object... objects) {
-//        boolean keyMode = false;
-//
-//        for (int i = 0; i < objects.length; i++) {
-//            if (objects[i] instanceof JPArg) {
-//                keyMode = true;
-//
-//                args.put(((JPArg) objects[i]).key, ((JPArg) objects[i]).object);
-//            } else if (keyMode){
-//                throw new RuntimeException("no key");
-//            } else {
-//                args.put(this.keys.get(i), objects[i]);
-//            }
-//        }
         this.args.putAll(to(this.keys.toArray(new String[0]), objects));
     }
 
