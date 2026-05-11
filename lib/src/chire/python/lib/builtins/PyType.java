188490e8b91bd48dbfe090d82617540b72ea0a20
炽热S <3531962676@qq.com>
2026-05-12 01:45:09 +0800

    新增PyType类型 -> (type) 内容未完成

---
+++ lib/src/chire/python/lib/builtins/PyType.java
@@ -0,0 +1,12 @@
+package chire.python.lib.builtins;
+
+/**
+ *         type(object) -> the object's type
+ *         type(name, bases, dict, **kwds) -> a new type
+ *         # (copied from class doc)
+ */
+public class PyType extends PyObject {
+    public PyType(String name, PyTuple bases, PyDict dict) {
+        //定义class
+    }
+}
