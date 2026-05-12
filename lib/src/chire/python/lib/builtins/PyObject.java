9a8cdfb958c298db34e221ed101f567d5bccdf88
LenovoSchool <114514@xb.com>
2026-05-12 17:06:28 +0800

    暂时修改protected -> public并设置__init__返回null

--- lib/src/chire/python/lib/builtins/PyObject.java
+++ lib/src/chire/python/lib/builtins/PyObject.java
@@ -1,20 +1,15 @@
 package chire.python.lib.builtins;
 
 import chire.python.lib.PyTypes;
-import chire.python.lib.escape.JPFunction;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.WeakHashMap;
 
 public class PyObject {
     /**关于私有变量，一般是存储在此*/
     public static PyDict __dict__;
     /**存储所有的数据，包括__dict__*/
-    protected final Map<String, PyObject> attrs = new HashMap<>();
+    public final Map<String, PyObject> attrs = new HashMap<>();
 
     static {
         PyTypes.initType();
@@ -24,8 +19,13 @@ public class PyObject {
         __init__(this);
     }
 
-    public static void __init__(PyObject self) {
+    /**
+     * @return null
+     */
+    public static PyObject __init__(PyObject self) {
         //TODO 关于py与java对应关系这方面我并没有寻思好该如何
-//        self.globals.update("__name__", "object");
+//        self.attrs.update("__name__", "object");
+
+        return null;
     }
 }
