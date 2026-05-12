febf3605fd4d8dee8ae122886c0bafb1bc14dd77
LenovoSchool <114514@xb.com>
2026-05-12 17:05:47 +0800

    简单添加PyClass构造方法，未完成

--- lib/src/chire/python/lib/types/PyClass.java
+++ lib/src/chire/python/lib/types/PyClass.java
@@ -1,6 +1,22 @@
 package chire.python.lib.types;
 
+import chire.python.lib.builtins.PyDict;
 import chire.python.lib.builtins.PyObject;
 
 public class PyClass extends PyObject {
+    public static PyDict __dict__ = new PyDict(){{
+        //TODO 未完成部分获取，可能需要新的设计代码
+    }};
+
+    public PyClass(Class<?> clazz) {
+
+    }
+
+    public static PyObject __init__(PyObject self) {
+        PyObject.__init__(self);
+
+        self.attrs.put("__dict__", new PyDict());
+
+        return null;
+    }
 }
--- lib/src/chire/python/lib/types/PyModule.java
+++ lib/src/chire/python/lib/types/PyModule.java
@@ -2,5 +2,8 @@ package chire.python.lib.types;
 
 import chire.python.lib.builtins.PyObject;
 
-public class PyModule extends PyObject {
+public class PyModule extends PyClass {
+    public PyModule(Class<?> clazz) {
+        super(clazz);
+    }
 }
